06ab79caf036bf3be1e8a493bf75fbfbb03ed522
sunghwan <namul10@gmail.com>
2014-12-22 22:07:27 +0900

    classify 구현완료. 벡터간의 거리를 구하는 공식을 사용한다.

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,13 +1,30 @@
 __author__ = 'josunghwan'
 
 from numpy import *
-
+import operator
 def createDataSet():
     group = array([[1, 1.1], [1.1, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+def classify(target, data_set, labels, k):
+    print data_set
+    data_set_size = data_set.shape[0]
+    diff_matrix = tile(target, (data_set_size, 1)) - data_set
+    diff_matrix = diff_matrix ** 2
+    diff_matrix = diff_matrix.sum(axis=1)
+    distance_matrix = diff_matrix ** 0.5
+    distance_indicies = distance_matrix.argsort()
+    class_count = {}
+    for i in range(k):
+        vote_label = labels[distance_indicies[i]]
+        class_count[vote_label] = class_count.get(vote_label, 0)+1
+    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
 
 if __name__=='__main__':
     group, labels = createDataSet()
-    print group
+    classify([1, 1.1], group, labels, 3)
+
+
